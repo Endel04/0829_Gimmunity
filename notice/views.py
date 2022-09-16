61f8d87b3aebb1aebbe9f16a75a14a227292739a
Halam Yu <w2037@e-mirim.hs.kr>
2022-09-16 11:18:18 +0900

    views, templates, urls notice:detail

--- notice/views.py
+++ notice/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from notice.models import Notice
 
 
 class NoticeListView(ListView):
     model = Notice  # DB에서 notice 전체 가져와서 notice_list라는 변수로 notice_list.html 파일에 전달
+
+
+class NoticeDetailView(DetailView):
+    model = Notice  # DB에서 pk에 해당하는 notice를 가져와서 notice라는 변수로 notice_detail.html 파일에 전달
